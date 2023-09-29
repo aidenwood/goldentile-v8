e445251f402d2c10e6a84c65473027acee6ccf41
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-09-29 10:55:05 +1000

    Morning Work

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,7 @@ module.exports = {
       current: 'currentColor',
       transparent: 'transparent',
       white: '#ffffff',
-      primary: colors.stone,
+      primary: colors.yellow,
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
